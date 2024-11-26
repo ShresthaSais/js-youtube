65c3d04aa308c32d85f4ec47d666b78000681f09
ShresthaSais <118791952+ShresthaSais@users.noreply.github.com>
2024-11-26 16:19:56 +0000

    dataType summary

---
+++ 01_basics/data_types.js
@@ -0,0 +1,22 @@
+// primitive
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+// Reference (non primitive)
+
+// Array, Objects (master objects, master web events and browser events then you'll have mastered JS - Chai aur code), Functions
+
+let myObj = {
+    name : "sais",
+    age : 25,
+
+
+}
+
+const myFunction = function () {
+    console.log("Hello World");
+    
+}
+
+// all non-primitive data types when returned using typeOf , it'll return object.
+
