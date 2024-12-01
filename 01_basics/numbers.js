5c2b46e94762c84cf3e56cc46ab427063598381d
ShresthaSais <118791952+ShresthaSais@users.noreply.github.com>
2024-12-01 18:12:25 +0000

    numbers and maths updated

--- 01_basics/numbers.js
+++ 01_basics/numbers.js
@@ -1,5 +1,19 @@
 const balance = new Number(100)
-console.log(balance);
+// console.log(balance);
 
 const otherNumber = 123.444
-console.log(otherNumber.toPrecision(2));
+// console.log(otherNumber.toPrecision(2));
+
+console.log(Math);
+
+console.log(Math.abs(-1));
+
+const randomNumber = console.log(Math.floor(Math.random(1, 9)*10) + 1)   // thisss
+
+// randomNumber = Math.floor()
+// console.log(randomNumber);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()*(max - min + 1)) + min);
