82dc35d6eac824147a72cd8825d58de6081cee2e
ShresthaSais <118791952+ShresthaSais@users.noreply.github.com>
2024-12-02 16:21:31 +0000

    array_JS

---
+++ 02_basics/array.js
@@ -0,0 +1,27 @@
+//array
+
+const myArr = [1, 2, 3, 4, 5]
+console.log(myArr);
+
+const myHeros = ["Spiderman", "Superman"]
+
+const myArr2 = new Array(1, 2, 3, 4)
+console.log(myArr[1]);
+
+// Array Methods
+
+myArr.push(6)
+console.log(myArr);
+
+myArr.pop() // remove the last value
+console.log(myArr);
+
+myArr.unshift(9) // adds the number at the beginning
+console.log(myArr);
+
+myArr.shift() //similar to pop() method
+ console.log(myArr.includes(2));
+
+// slice and splice
+
+
