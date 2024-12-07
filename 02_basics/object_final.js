06bf3f2077ef5f9e6896765c5defcf949b41ded7
ShresthaSais <118791952+ShresthaSais@users.noreply.github.com>
2024-12-07 17:01:21 +0000

    objects

---
+++ 02_basics/object_final.js
@@ -0,0 +1,14 @@
+const course = {
+    name : "sais",
+    coursename : "JS in English",
+    price : 999
+
+}
+
+const {coursename : coursenamee} = course // destructring objects. Also used for arrays
+
+console.log(coursenamee);
+
+/*destructring in React 
+const navbar = ({company}) => {}
+*/    
